e4f3b4dcad26c3440587fb2d9c479c5915df2e2f
louisbl <louis@beltramo.me>
2018-02-20 18:30:20 +0100

    Add Login button

--- app/src/main/java/fr/louisbl/ddi16/architecturecomponents/MainActivity.java
+++ app/src/main/java/fr/louisbl/ddi16/architecturecomponents/MainActivity.java
@@ -1,13 +1,53 @@
 package fr.louisbl.ddi16.architecturecomponents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "00b7d581f8ef4bb184ee94d055659762"; // FIXME
+    private static final int REQUEST_CODE = 1001;
+    private static final String REDIRECT_URI = "ddi16testapp://callback"; // FIXME
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthenticationRequest.Builder builder =
+                        new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+                builder.setScopes(new String[]{"streaming"});
+                AuthenticationRequest request = builder.build();
+
+                AuthenticationClient.openLoginActivity(MainActivity.this, REQUEST_CODE, request);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Log.d("AccessToken", response.getAccessToken());
+            }
+        }
+    }
+
 }
