cf04ab60cb4f8069f4c90e8e4befe7e3129300a5
louisbl <louis@beltramo.me>
2018-02-21 17:56:55 +0100

    Simple in-memory cache

--- app/src/main/java/fr/louisbl/ddi16/architecturecomponents/ArtistRepository.java
+++ app/src/main/java/fr/louisbl/ddi16/architecturecomponents/ArtistRepository.java
@@ -4,6 +4,8 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.util.Log;
 
+import java.util.HashMap;
+
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyCallback;
 import kaaes.spotify.webapi.android.SpotifyError;
@@ -17,8 +19,15 @@ import retrofit.client.Response;
 
 public class ArtistRepository {
 
+    private static final HashMap<String, LiveData<ArtistEntity>> simpleCache = new HashMap<>();
+
     public LiveData<ArtistEntity> getArtistById(String artistId) {
+        LiveData<ArtistEntity> cached = simpleCache.get(artistId);
+        if (cached != null) return cached;
+
         final MutableLiveData<ArtistEntity> data = new MutableLiveData<>();
+        simpleCache.put(artistId, data);
+
         SpotifyApi api = new SpotifyApi();
         api.setAccessToken(MainActivity.TOKEN);
 
@@ -33,6 +42,8 @@ public class ArtistRepository {
             @Override
             public void success(Artist artist, Response response) {
                 Log.d("Artist", artist.name);
+                // data.setValue() _must_ be called from main Thread
+                // data.postValue() always run on the main Thread
                 data.postValue(new ArtistEntity(artist));
             }
         });
